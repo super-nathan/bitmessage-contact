cc517c77f2ee90eef85a9ae277ffb42e508d7524
root <root@d1stkfactory>
2013-08-19 07:37:22 +0000

    cleaning

--- send_contact.php
+++ send_contact.php
@@ -5,15 +5,10 @@ $name = $_POST['name'];
 $contact = $_POST['contact'];
 
 $signature="\n"."\n".$name." ".$contact;
-
 $message=$detail." ".$signature;
 
 $var1= '"'.$subject.'"';
 $var2= '"'.$message.'"';
-
-$pyvar=$var1." ".$var2;
-
-$pyvar= '"'.$pyvar.'"';
 
 $send_contact=exec("python bm.py $var1 $var2 ");
 
